52904b957536df5e9e83f00dcbf3e7f6496053aa
Said Maadan <me@saidmaadan.com>
2020-10-12 11:45:46 -0500

    Created menu navbar and sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+
+    def inline_svg_tag filename, options={}
+        file = File.read(Rails.root.join('app', 'assets', 'images', 'svg', filename))
+        #file = File.open(`https://inventive-labs.s3.amazonaws.com/img/svg/#{filename}`) { |file| raw file.read }
+        # file = localFile || remoteFile
+        #file = File.read("https://inventive-labs.s3.amazonaws.com/img/svg/#{filename}")
+        doc = Nokogiri::HTML::DocumentFragment.parse file
+        svg = doc.at_css 'svg'
+        if options[:class].present?
+            svg['class'] = options[:class]
+        end
+        doc.to_html.html_safe
+    end
+    
     def avatar_url(agent)
         if agent.avatar.attached?
             url_for(agent.avatar)
